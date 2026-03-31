1043f11f3b63dc2cf36c5d1166471996bfdb19b4
Sresta <sresta078@academiacollege.edu.np>
2026-03-31 15:24:50 +0545

    feat: added search debouncing

--- src/components/common/Search.tsx
+++ src/components/common/Search.tsx
@@ -1,21 +1,36 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useWeatherStore } from "../../store/weatherStore"
 
 import { Input } from "@/components/ui/input"
 import { Button } from "@/components/ui/button"
 
 export default function Search() {
-
   const [city, setCity] = useState("")
+  const [lastSearched, setLastSearched] = useState("")
+
   const fetchWeather = useWeatherStore((state) => state.fetchWeather)
 
-  const handleSearch = () => {
-    if (!city.trim()) return
-    fetchWeather({ city })
+  const trimmedCity = city.trim()
+
+  // Handle Search
+  const searchCity = () => {
+    if (trimmedCity.length < 2) return
+    if (trimmedCity === lastSearched) return
+
+    fetchWeather({ city: trimmedCity })
+    setLastSearched(trimmedCity)
   }
 
-  return (
+  // Search Debouncing
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      searchCity()
+    }, 500)
 
+    return () => clearTimeout(timer)
+  }, [trimmedCity, lastSearched])
+
+  return (
     <div className="flex flex-col sm:flex-row gap-2 w-full max-w-md">
 
       <Input
@@ -23,13 +38,14 @@ export default function Search() {
         value={city}
         onChange={(e) => setCity(e.target.value)}
         onKeyDown={(e) => {
-          if (e.key === "Enter") handleSearch()
+          if (e.key === "Enter") searchCity()
         }}
         className="rounded-3xl bg-white/20 border-white/30 text-white placeholder:text-gray-300 focus-visible:ring-0 focus-visible:ring-offset-0"
       />
 
-      <Button type="button"
-        onClick={handleSearch}
+      <Button
+        type="button"
+        onClick={searchCity}
         className="rounded-3xl bg-blue-700 hover:bg-blue-600 w-full sm:w-auto cursor-pointer"
       >
         Search
